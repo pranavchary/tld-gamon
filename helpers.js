a92f8ff3094e5f09ebac12d0d5aaf7bc4b6fb5ec
Pranav Chary <pranavchary.dev@gmail.com>
2023-10-22 13:25:25 -0500

    Code cleanup: Add constants to a separate file

--- helpers.js
+++ helpers.js
@@ -1,52 +1,4 @@
-if (!process.env.NODE_ENV) require('dotenv').config();
-const { env: { GAMON_SHOUT_ID, STORM_BOLT_ID, GOREHOWL_ID } } = process;
-const BASE_SCORE_LEVEL = 7.5;
-const BASE_SCORE_COMPLETION = 37.5;
-const dungeonShortnameMap = {
-    BH: 'Brackenhide Hollow',
-    FH: 'Freehold',
-    HOI: 'Halls of Infusion',
-    NL: 'Neltharion\'s Lair',
-    NELT: 'Neltharus',
-    UNDR: 'Underrot',
-    VP: 'Vortex Pinnacle',
-    ULD: 'Uldaman'
-};
-const specNameMap = {
-    Affliction: 'Aff',
-    Assassination: 'Sin',
-    'Beast Mastery': 'BM',
-    Brewmaster: 'BM',
-    Augmentation: 'Aug',
-    Demonology: 'Demo',
-    Destruction: 'Destro',
-    Devastation: 'Dev',
-    Discipline: 'Disc',
-    Elemental: 'Ele',
-    Enhancement: 'Enhance',
-    Marksmanship: 'MM',
-    Mistweaver: "MW",
-    Preservation: 'Pres',
-    Protection: 'Prot',
-    Restoration: 'Resto',
-    Retribution: 'Ret',
-    Subtlety: 'Sub',
-    Windwalker: 'WW'
-};
-
-const sayQuotes = [
-    'Many times have I fallen at the hands of an orc and many times have I risen again.',
-    'For every orc that struck me, I will cleave a thousand of their skulls! This ends here!',
-    'For the Horde, my friend.',
-    'I may be bruised and beaten, but the hatred boils inside me.'
-];
-
-const shoutQuotes = [
-    'THIS ENDS HERE!',
-    `<:gamon_shout:${GAMON_SHOUT_ID}> I, GAMON, WILL SAVE US!`,
-    `<:gorehowl:${GOREHOWL_ID}> TASTE MY AXE!`,
-    `<:storm_bolt:${STORM_BOLT_ID}> I AM A STORM OF PAIN!`,
-];
+const { BASE_SCORE_LEVEL, BASE_SCORE_COMPLETION } = require('./constants');
 
 /**
  * @param {string} text The text to capitalize
@@ -136,9 +88,5 @@ module.exports = {
     capitalizeText,
     sanitizeNumber,
     getDungeonRating,
-    getTargetKeystoneLevel,
-    dungeonShortnameMap,
-    specNameMap,
-    sayQuotes,
-    shoutQuotes
+    getTargetKeystoneLevel
 };
